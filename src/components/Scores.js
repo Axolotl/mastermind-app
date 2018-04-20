929a76f5fa1799de6ed528b758f82c2483e1c87c
Axolotl <samuelmachat@gmail.com>
2018-04-19 22:38:06 -0400

    cleaned up scores.js

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -27,39 +27,3 @@ const Scores = ({ data }) => (
 
 
 export default Scores
-
-
-// const Board = ({ data }) => (
-//   <Wrapper>
-
-//     {Object.entries(data).map((set, i) => (
-//       <div key={i} style={{margin: '20px 15px 20px 15px'}}>
-//         <p style={{margin: '5px', fontSize: '1em',}}>
-//           {set[0]}
-//         </p>
-//         <table>
-//           <tbody>
-//             {set[1].map((datum, i) => (
-//               <tr key={i}>
-//                 <td>
-//                   <Wrapper>
-//                     {datum &&
-//                       datum.map((color, index) => {
-//                         return (
-//                           <Circle key={index} color={color} size='10px' margin='0px 5px'/>
-//                         )
-//                       })
-//                     }
-//                   </Wrapper>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     ))}
-
-//   </Wrapper>
-// )
-
-// export default Board
