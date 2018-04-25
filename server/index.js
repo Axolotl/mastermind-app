850808d811b16fd32fbb1dbf967c06985e0fe4ae
Axolotl <samuelmachat@gmail.com>
2018-04-24 22:07:29 -0400

    fixed db issue

--- server/index.js
+++ server/index.js
@@ -21,9 +21,9 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
 
 
 //
